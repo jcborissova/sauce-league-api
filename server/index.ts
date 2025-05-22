fcf6caf33728a55cc2ea8ca3515b630788ab4a02
Juan Carlos Borissova <borissova706@gmail.com>
2025-05-22 13:03:28 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import { playerRouter } from "./routes/players.js";
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
